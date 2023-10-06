fc580b5345680554dc793ea78bc3a053c254e5e0
Serhiy Davydko <serhiy.davydko@corezoid.com>
2023-10-06 17:00:16 +0300

    fix not found

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,49 @@
-import Link from 'next/link';
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import SEO from '@/components/chunks/SEO';
+import { useTranslate } from '@/hooks/useTranslate';
+import Button from '@/components/UI/Button';
+import Layout from '../components/chunks/Layout';
+import HeaderTitle from '../components/chunks/HeaderTitle';
+import TextUnderlied from '../components/chunks/TextUnderlied';
 
-import style from './404.module.scss';
+import styles from './404.module.scss';
 
-const NotFound: React.FC = () => (
-  <div className={style.mainContainer}>
-    <div className={style.textCenter}>
-      <h1>404</h1>
-      <h3>PAGE NOT FOUND</h3>
-      <Link href="/" passHref>
-        <button type="button" name="button">Back To Home</button>
-      </Link>
-    </div>
-  </div>
-);
+const NotFound: React.FC = () => {
+  const { t } = useTranslate();
+  const router = useRouter();
+
+  const goToContactUs = () => {
+    router.push('/contact');
+  };
+
+  const goToMain = () => {
+    router.push('/');
+  };
+
+  return (
+    <Layout>
+      <SEO />
+      <main className={styles.main}>
+        <div className={styles.mainContent}>
+          <HeaderTitle>
+            <span className={styles.mainTitle}>
+              4
+              <TextUnderlied>0</TextUnderlied>
+              4
+            </span>
+          </HeaderTitle>
+          <div className={styles.mainDescription}>
+            {t('notFound.description')}
+          </div>
+          <div className={styles.buttonGroup}>
+            <Button style={{ width: 160 }} type="outlined" onClick={goToContactUs}>Contact Us</Button>
+            <Button style={{ width: 160 }} type="primary" onClick={goToMain}>Home</Button>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+};
 
 export default NotFound;
